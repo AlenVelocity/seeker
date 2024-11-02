4c467a78498ae45520ae96b2816267171b3b1088
AlenVelocity <alenyohannan71@gmail.com>
2024-11-03 00:58:00 +0530

    fix(): uploadthing router

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,17 +1,11 @@
 import { createRouteHandler } from 'uploadthing/next'
 
 import { ourFileRouter } from './core'
-import { NextRequest } from 'next/server'
 
 // Export routes for Next App Router
-export const { GET, POST: utPost } = createRouteHandler({
+export const { GET, POST } = createRouteHandler({
     router: ourFileRouter
 
     // Apply an (optional) custom config:
     // config: { ... },
 })
-
-export const POST = (req: NextRequest) => {
-    console.log('POST', req)
-    return utPost(req)
-}
